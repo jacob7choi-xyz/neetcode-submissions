8a04863302ebdf3b73b1acbdacbbc98ee249be58
Jacob J. Choi <jacob77choi@gmail.com>
2026-08-10 15:20:11 -0500

    Add: n-queens - submission-0

---
+++ Data Structures & Algorithms/n-queens/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        res = []
+        col, diag, anti_diag = set(), set(), set()
+        def dfs(index: int, current: List[str]) -> None:
+            if index == n:
+                res.append(current.copy())
+                return
+            for i in range(n):
+                if i in col or (index - i) in diag or (index + i) in anti_diag:
+                    continue
+                col.add(i)
+                diag.add(index - i)
+                anti_diag.add(index + i)
+                current.append(('.' * i) + 'Q' + '.' * (n - i - 1))
+                dfs(index + 1, current)
+                col.remove(i)
+                diag.remove(index - i)
+                anti_diag.remove(index + i)
+                current.pop()
+        dfs(0, [])
+        return res
